d5b54022a704e4fdb3187911877c04cb5c09764e
Brynnon Walker <brynnonw@gmail.com>
2018-06-29 07:20:04 -0500

    add head

--- brynnonbrew.rb
+++ brynnonbrew.rb
@@ -4,7 +4,8 @@ class Brynnonbrew < Formula
     url "https://github.com/BrynnonWalker/BrynnonBrew/archive/v1.0.tar.gz"
     version "1.0"
     sha256 "54fba27d70cef9c2c218b634c5c0275bf390a8abe2622751b4fc1499f78c2645"
-
+    head "https://github.com/BrynnonWalker/BrynnonBrew"
+    
     def install
         prefix.install "bin", "data", "lib", "vendor", "LEGAL", "NEWS"
     end
