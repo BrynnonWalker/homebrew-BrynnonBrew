aa364c58248facf8d97e8030719b8fd373d08680
Brynnon Walker <brynnonw@gmail.com>
2018-06-29 07:24:00 -0500

    remove head, update sha

--- brynnonbrew.rb
+++ brynnonbrew.rb
@@ -1,14 +1,9 @@
 class Brynnonbrew < Formula
     desc "Brynnon-Brew, a test tap for Homebrew"
     homepage ""
-    url "https://github.com/BrynnonWalker/BrynnonBrew/archive/v1.0.tar.gz"
-    version "1.0"
-    sha256 "54fba27d70cef9c2c218b634c5c0275bf390a8abe2622751b4fc1499f78c2645"
-    
-    head do 
-        url "https://github.com/BrynnonWalker/BrynnonBrew"
-        sha256 "911c29b3889e07290aaae17b536ea52619622fe30e6b2e4577ed351948b7430c"
-    end
+    url "https://github.com/BrynnonWalker/BrynnonBrew/archive/v1.1.tar.gz"
+    version "1.1"
+    sha256 "37f1e0fa541cae0c7f16f8c2cb5a5fdfcbebcbff23bbe9e73ab062f292ae2d22"
     
     def install
         prefix.install "bin", "data", "lib", "vendor", "LEGAL", "NEWS"
