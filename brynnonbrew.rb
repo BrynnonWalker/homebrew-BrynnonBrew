bcd9bc5ccd5ab605d1a346137c09194df3cce8fd
Brynnon Walker <brynnonw@gmail.com>
2018-06-29 07:29:27 -0500

    update target release/sha

--- brynnonbrew.rb
+++ brynnonbrew.rb
@@ -2,8 +2,8 @@ class Brynnonbrew < Formula
     desc "Brynnon-Brew, a test tap for Homebrew"
     homepage ""
     url "https://github.com/BrynnonWalker/BrynnonBrew/archive/v1.1.tar.gz"
-    version "1.1"
-    sha256 "37f1e0fa541cae0c7f16f8c2cb5a5fdfcbebcbff23bbe9e73ab062f292ae2d22"
+    version "1.0"
+    sha256 "4e26e41eb541da28c45cf3888adda0f50075c073572b009c56cbe6e9d2aab652"
     
     def install
         prefix.install "bin", "data", "lib", "vendor", "LEGAL", "NEWS"
