7c10df53773e95dce3ca82dd8944491fd4a743d5
Brynnon Walker <brynnonw@gmail.com>
2018-06-29 08:49:26 -0500

    update sha

--- brynnonbrew.rb
+++ brynnonbrew.rb
@@ -3,7 +3,7 @@ class Brynnonbrew < Formula
     homepage ""
     url "https://github.com/BrynnonWalker/BrynnonBrew/archive/v1.0.tar.gz"
     version "1.0"
-    sha256 "e7d8ab1faaeae0a7da48a0591d7cd3559bd754f5a8704c7132498113cc4ae0d6"
+    sha256 "d0d709dbd7d4030c4cfffb8a36b830e0974cbc907cb092eec16088ced6609b45"
     
     def install
         prefix.install "bin", "data", "lib", "vendor", "LEGAL", "NEWS"
