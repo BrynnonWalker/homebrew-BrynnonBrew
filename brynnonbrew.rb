5594bbccd55320dd1ceacc173a5f503a6bd450f6
Brynnon Walker <brynnonw@gmail.com>
2018-06-29 07:29:49 -0500

    update target release

--- brynnonbrew.rb
+++ brynnonbrew.rb
@@ -1,7 +1,7 @@
 class Brynnonbrew < Formula
     desc "Brynnon-Brew, a test tap for Homebrew"
     homepage ""
-    url "https://github.com/BrynnonWalker/BrynnonBrew/archive/v1.1.tar.gz"
+    url "https://github.com/BrynnonWalker/BrynnonBrew/archive/v1.0.tar.gz"
     version "1.0"
     sha256 "4e26e41eb541da28c45cf3888adda0f50075c073572b009c56cbe6e9d2aab652"
     
